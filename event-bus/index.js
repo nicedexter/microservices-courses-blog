ddd4d617a0e0c69a66a63be47490450e0767253c
Manuel Spuhler <manuel.spuhler@gmail.com>
2020-06-28 23:56:50 +0200

    Fix ports

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,13 +9,14 @@ const events = [];
 
 app.post("/events", (req, res) => {
   const event = req.body;
+  console.log('event-bus: ', event)
 
   events.push(event);
 
   axios.post("http://posts-clusterip-srv:4000/events", event);
-  axios.post("http://moderation-srv:4001/events", event);
+  axios.post("http://moderation-srv:4003/events", event);
   axios.post("http://query-srv:4002/events", event);
-  axios.post("http://comments-srv:4003/events", event);
+  axios.post("http://comments-srv:4001/events", event);
 
   res.send({ status: "OK" });
 });
